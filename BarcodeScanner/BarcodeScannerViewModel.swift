e6bc4ab882c1363b57c0eb883057992e825421d5
sbibla <saar.bibla@gmail.com>
2024-04-16 20:56:23 -0700

    Working pre-project organization

---
+++ BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  BarcodeScannerViewModel.swift
+//  BarcodeScanner
+//
+//  Created by Saar Bibla on 4/15/24.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        return scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
